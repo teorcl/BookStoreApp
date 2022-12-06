9cae1897f2e1e671c26458bd1a1430fd25179493
Teodoro <teo.calle.lara@gmail.com>
2022-12-06 16:44:27 -0500

    Se construyen las vistas

--- BookStoreApp/SalesViewController.swift
+++ BookStoreApp/SalesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SalesViewController.swift
 //  BookStoreApp
 //
 //  Created by TEO on 6/12/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SalesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
